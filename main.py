19a9de9d82d84b58ad87ae9af9845da5f46216e2
zuzagasik <zuzagasik@gmail.com>
2026-01-17 15:57:00 +0100

    import tkinter, otworzenie okna aplikacji i zaladowanie interfejsu

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-# to be done
+import tkinter as tk
+from app.gui.main_window import WeatherAppUI
+
+def main():
+    """
+    Główna funkcja uruchamiająca aplikację pogodową.
+    """
+    # Tworzenie głównego okna instancji Tkinter
+    root = tk.Tk()
+    
+    # Ustawienie tytułu okna aplikacji
+    root.title("Aplikacja Pogodowa")
+    
+    # Inicjalizacja interfejsu użytkownika
+    # Przekazujemy 'root' do klasy, która zarządza widokiem
+    app = WeatherAppUI(root)
+    
+    # Uruchomienie pętli zdarzeń (aplikacja czeka na interakcję użytkownika)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
